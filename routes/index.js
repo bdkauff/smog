f355cd1e9302098d9d0a88b3eb5960434099c96c
Sarah Rothberg <sarahrothberg@gmail.com>
2014-04-06 00:39:03 -0400

    switched name order changed somethign with kelsas

--- routes/index.js
+++ routes/index.js
@@ -16,28 +16,28 @@ exports.smogIssue = function(req,res) {
 }
 
 exports.adam_mann = function(req,res) {
-	res.render('adam_mann', {title: 'Adam Mann|SMOG'});
+	res.render('adam_mann', {title: 'SMOG|Adam Mann'});
 }
 
 exports.seth_maxon = function(req,res) {
-	res.render('seth_maxon', {title: 'Seth Maxon|SMOG'});
+	res.render('seth_maxon', {title: 'SMOG|Seth Maxon'});
 }
 
 exports.kelsa_trom = function(req,res) {
-	res.render('kelsa_trom', {title: 'Kelsa Trom|SMOG'});
+	res.render('kelsa_trom', {title: 'SMOG|Kelsa Trom'});
 
 }
 
 exports.sophia_lefraga = function(req,res) {
-	res.render('sophia_lefraga', {title: 'Sophia Le Fraga|SMOG'});
+	res.render('sophia_lefraga', {title: 'SMOG|Sophia Le Fraga'});
 }
 
 exports.andy_scott = function(req,res) {
-	res.render('andy_scott', {title: 'Andrea Scott|SMOG'});
+	res.render('andy_scott', {title: 'SMOG|Andrea Scott'});
 }
 
 exports.travis_korte = function(req,res) {
-	res.render('travis_korte', {title: 'Travis Korte|SMOG'});
+	res.render('travis_korte', {title: 'SMOG|Travis Korte'});
 }
 exports.anita_pan = function(req,res) {
 	res.render('anita_pan', {title: 'Anita Pan|SMOG'});
